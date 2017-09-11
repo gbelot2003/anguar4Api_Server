0378d546a8a89af9fa27e1b6bec2f5d3d750b2d9
Gerardo Belot <gbelot2003@hotmail.com>
2017-09-11 16:40:14 -0600

    creando acciones de controller y routing en API

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Image;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    //
+    public function index()
+    {
+        $images = Image::all();
+        return Response::json($images, 200);
+    }
+
+    public function store(Request $request)
+    {}
+
+    public function edit($id)
+    {    }
+
+    public function update(Request $request, $id)
+    {}
+
+    public function delete($id)
+    {}
 }
